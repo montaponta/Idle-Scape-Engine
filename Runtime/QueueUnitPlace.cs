a8e1ad0a3ef4dc753db6477a38cbd3e36bd72b7e
ositnikov <mr.moonshield@yandex.ru>
2024-04-25 13:31:21 +0300

    1.5.0

--- Runtime/QueueUnitPlace.cs
+++ Runtime/QueueUnitPlace.cs
@@ -3,56 +3,58 @@ using UnityEngine;
 
 public class QueueUnitPlace : MainRefs
 {
-	public ResourceType type;
-	protected List<AbstractUnit> unitsList = new List<AbstractUnit>();
-	public int queueLength = 1;
+    public ResourceType type;
+    protected List<AbstractUnit> unitsList = new List<AbstractUnit>();
+    public int queueLength = 1;
 
-	public virtual bool CanEnter(AbstractUnit unit)
-	{
-		var exist = unitsList.Contains(unit);
-		if (!exist) unitsList.Add(unit);
+    public virtual bool CanEnter(AbstractUnit unit)
+    {
+        var exist = unitsList.Contains(unit);
+        if (!exist) unitsList.Add(unit);
 
-		for (int i = 0; i < queueLength; i++)
-		{
-			if (unitsList[i] == unit) return true;
-		}
+        for (int i = 0; i < queueLength; i++)
+        {
+            if (unitsList[i] == unit) return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	public virtual void RemoveUnit(AbstractUnit unit)
-	{
-		unitsList.Remove(unit);
-	}
+    public virtual void RemoveUnit(AbstractUnit unit)
+    {
+        unitsList.Remove(unit);
+    }
 
-	public virtual float GetDistance(AbstractUnit unit)
-	{
-		return Vector3.Distance(transform.position, unit.transform.position);
-	}
+    public virtual float GetDistance(AbstractUnit unit)
+    {
+        return Vector3.Distance(transform.position, unit.transform.position);
+    }
 
-	public virtual bool IsOccupiedPlace()
-	{
-		return unitsList.Count >= GetQueueLength();
-	}
+    public virtual bool IsOccupiedPlace()
+    {
+        return unitsList.Count >= GetQueueLength();
+    }
 
-	public virtual int GetEmptyPlaces()
-	{
-		return GetQueueLength() - unitsList.Count;
-	}
+    public virtual int GetEmptyPlaces()
+    {
+        return GetQueueLength() - unitsList.Count;
+    }
 
-	public virtual int GetQueueLength()
-	{
-		return queueLength;
-	}
+    public virtual int GetQueueLength()
+    {
+        return queueLength;
+    }
 
-	public virtual void OnQueuePlaceReached(AbstractUnit unit) { }
+    public virtual void OnQueuePlaceReached(AbstractUnit unit) { }
 
-	public List<AbstractUnit> GetUnitsList()
-	{
-		return unitsList;
-	}
+    public List<AbstractUnit> GetUnitsList()
+    {
+        return unitsList;
+    }
 
     public virtual Transform GetTargetPoint(AbstractUnit unit) { return transform; }
+    public virtual Transform GetLastTargetPoint() { return transform; }
+    public virtual Transform GetLastFreeTargetPoint() { return transform; }
 
     public virtual int GetMyQueueIndex(AbstractUnit unit)
     {
