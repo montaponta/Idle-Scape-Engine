0070ed2a56a2fc7a9705d02946a68ace380ab715
ositnikov <mr.moonshield@yandex.ru>
2024-07-09 15:32:02 +0300

    5.3.0

--- Abstracts/AbstractTransport.cs
+++ Abstracts/AbstractTransport.cs
@@ -3,108 +3,110 @@ using UnityEngine;
 
 public abstract class AbstractTransport : QueueUnitPlace, IIDExtention
 {
-	public TrasportTargetType targetType;
-	public string id;
-	public Transform unitsFolder;
-	public Action<AbstractTransport> OnTransportReachedDestination;
-	[NonSerialized] public Vector3 defaultPos, defaultRot, targetPos, targetRot;
-	[NonSerialized] public float stateIndex;
-	protected int seatsBusyCount;
-	protected Main main => GetRef<Main>();
-	protected AbstractSavingManager savingManager => GetRef<AbstractSavingManager>();
+    public TrasportTargetType targetType;
+    public string id;
+    public Transform unitsFolder;
+    public Action<AbstractTransport> OnTransportReachedDestination;
+    [NonSerialized] public Vector3 defaultPos, defaultRot, targetPos, targetRot;
+    [NonSerialized] public float stateIndex;
+    protected int seatsBusyCount;
+    protected Main main => GetRef<Main>();
+    protected AbstractSavingManager savingManager => GetRef<AbstractSavingManager>();
 
-	public virtual void OnTransportEnter(AbstractUnit unit)
-	{
-		seatsBusyCount++;
-		int index = 0;
-		var tr = GetComponent<AbstractCraftItem>().GetCurrentLevelTransform();
-		Transform pointsFolder = main.GetTransformChild<Transform>(tr, "PointsFolder");
+    public virtual void Init() { }
 
-		foreach (Transform item in pointsFolder)
-		{
-			if (item.childCount > 0) continue;
-			unit.agent.enabled = false;
-			unit.transform.SetParent(item);
-			unit.transform.localPosition = Vector3.zero;
-			unit.transform.localRotation = Quaternion.identity;
-			SetUnitScale(unit, index);
-			index++;
-			break;
-		}
-	}
+    public virtual void OnTransportEnter(AbstractUnit unit)
+    {
+        seatsBusyCount++;
+        int index = 0;
+        var tr = GetComponent<AbstractCraftItem>().GetCurrentLevelTransform();
+        Transform pointsFolder = main.GetTransformChild<Transform>(tr, "PointsFolder");
 
-	public virtual void OnTransportExit(AbstractUnit unit, bool isExitAllUnits = false)
-	{
-		unit.transform.localScale = Vector3.one;
-		unit.transform.SetParent(unitsFolder);
-		unit.agent.enabled = true;
-		var pos = main.GetAgentCirclePosition(unit.transform, 3, unit.transform, 5);
-		unit.agent.Warp(pos);
-		unit.transform.eulerAngles = new Vector3(0, UnityEngine.Random.Range(0, 360), 0);
+        foreach (Transform item in pointsFolder)
+        {
+            if (item.childCount > 0) continue;
+            unit.agent.enabled = false;
+            unit.transform.SetParent(item);
+            unit.transform.localPosition = Vector3.zero;
+            unit.transform.localRotation = Quaternion.identity;
+            SetUnitScale(unit, index);
+            index++;
+            break;
+        }
+    }
 
-		main.StartCoroutine(main.ActionCoroutine(() =>
-		{
-			RemoveUnit(unit);
-			seatsBusyCount--;
-			SaveTransportState();
-		}, 0.02f));
-	}
+    public virtual void OnTransportExit(AbstractUnit unit, bool isExitAllUnits = false)
+    {
+        unit.transform.localScale = Vector3.one;
+        unit.transform.SetParent(unitsFolder);
+        unit.agent.enabled = true;
+        var pos = main.GetAgentCirclePosition(unit.transform, 3, unit.transform, 5);
+        unit.agent.Warp(pos);
+        unit.transform.eulerAngles = new Vector3(0, UnityEngine.Random.Range(0, 360), 0);
 
-	public abstract void SaveTransportState();
+        main.StartCoroutine(main.ActionCoroutine(() =>
+        {
+            RemoveUnit(unit);
+            seatsBusyCount--;
+            SaveTransportState();
+        }, 0.02f));
+    }
 
-	public virtual void ExitTransportAllUnits()
-	{
-		foreach (var unit in unitsList)
-		{
-			OnTransportExit(unit, true);
-		}
-	}
+    public abstract void SaveTransportState();
 
-	public override bool CanEnter(AbstractUnit unit)
-	{
-		if (unitsList.Contains(unit)) return true;
-		if (unitsList.Count >= queueLength) return false;
-		return base.CanEnter(unit);
-	}
+    public virtual void ExitTransportAllUnits()
+    {
+        foreach (var unit in unitsList)
+        {
+            OnTransportExit(unit, true);
+        }
+    }
 
-	public virtual void MoveToTarget(Vector3 point)
-	{
-		targetPos = point;
-	}
+    public override bool CanEnter(AbstractUnit unit)
+    {
+        if (unitsList.Contains(unit)) return true;
+        if (unitsList.Count >= queueLength) return false;
+        return base.CanEnter(unit);
+    }
 
-	protected virtual void SetUnitScale(AbstractUnit collector, int placeIndex) { }
+    public virtual void MoveToTarget(Vector3 point)
+    {
+        targetPos = point;
+    }
 
-	public int GetFreeSpace()
-	{
-		return queueLength - unitsList.Count;
-	}
+    protected virtual void SetUnitScale(AbstractUnit collector, int placeIndex) { }
 
-	public override void OnQueuePlaceReached(AbstractUnit unit)
-	{
-		StartCoroutine(main.ActionCoroutine(() => { OnTransportEnter(unit); }, () => unit.agent.isStopped));
-	}
+    public int GetFreeSpace()
+    {
+        return queueLength - unitsList.Count;
+    }
 
-	public virtual void TurnOnRevers(bool b) { }
-	public virtual bool IsSaveAndLoadPosition() { return true; }
-	public virtual int GetBusySeatsCount() { return seatsBusyCount; }
+    public override void OnQueuePlaceReached(AbstractUnit unit)
+    {
+        StartCoroutine(main.ActionCoroutine(() => { OnTransportEnter(unit); }, () => unit.agent.isStopped));
+    }
 
-	public void SetID(string id)
-	{
-		this.id = id;
-	}
+    public virtual void TurnOnRevers(bool b) { }
+    public virtual bool IsSaveAndLoadPosition() { return true; }
+    public virtual int GetBusySeatsCount() { return seatsBusyCount; }
 
-	public string GetID()
-	{
-		return id;
-	}
+    public void SetID(string id)
+    {
+        this.id = id;
+    }
 
-	public GameObject GetGameObject()
-	{
-		return gameObject;
-	}
+    public string GetID()
+    {
+        return id;
+    }
 
-	public UnityEngine.Object GetObject()
-	{
-		return this;
-	}
+    public GameObject GetGameObject()
+    {
+        return gameObject;
+    }
+
+    public UnityEngine.Object GetObject()
+    {
+        return this;
+    }
 }
