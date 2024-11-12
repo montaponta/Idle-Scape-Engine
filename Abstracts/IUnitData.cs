93f3591c9280fd485371274b118a2236e00d82b6
ositnikov <mr.moonshield@yandex.ru>
2024-11-12 15:04:12 +0300

    8.3.0

--- Abstracts/IUnitData.cs
+++ Abstracts/IUnitData.cs
@@ -3,4 +3,5 @@ public interface IUnitData
 	public int GetHealth();
 	public float GetMoveSpeed();
 	public object GetValueByTag(string tag, System.Type returnType);
+	public T GetValueByTag<T>(string tag);
 }
