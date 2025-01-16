a43c71a9f997e7d81da316e9c2a0774836ab2a47
ositnikov <mr.moonshield@yandex.ru>
2025-01-16 12:50:53 +0300

    10.5.1

--- SavingManager/AbstractSavingData.cs
+++ SavingManager/AbstractSavingData.cs
@@ -2,51 +2,52 @@ using UnityEngine;
 
 public abstract class AbstractSavingData
 {
-    protected AbstractSavingManager savingManager;
-    private Coroutine coroutine;
+	protected AbstractSavingManager savingManager;
+	private Coroutine coroutine;
 
-    public virtual void Init(AbstractSavingManager savingManager)
-    {
-        this.savingManager = savingManager;
-    }
+	public virtual void Init(AbstractSavingManager savingManager)
+	{
+		this.savingManager = savingManager;
+	}
 
-    public virtual void SaveData(bool collectParams, bool isSave = true)
-    {
-        if (savingManager.dontSave) return;
+	public virtual void SaveData(bool collectParams, bool isSave = true)
+	{
+		if (savingManager.dontSave) return;
+		var main = savingManager.GetRef<Main>();
 
-        if (!savingManager.GetRef<Main>().debugTools.isDebugActive)
-        {
-            if (coroutine != null) savingManager.StopCoroutine(coroutine);
+		if (!main.debugTools.isDebugActive)
+		{
+			if (coroutine != null) main.StopCoroutine(coroutine);
 
-            coroutine = savingManager.GetRef<Main>().Invoke(() =>
-            {
-                SaveDataObject();
-                coroutine = null;
-            }, 0.5f);
-        }
-        else if (isSave) SaveDataObject();
-    }
+			coroutine = main.Invoke(() =>
+			{
+				SaveDataObject();
+				coroutine = null;
+			}, 0.5f);
+		}
+		else if (isSave) SaveDataObject();
+	}
 
-    public virtual void SaveDataToPath(bool collectParams, string key)
-    {
-        SaveData(collectParams, false);
-        SaveDataObject(key);
-    }
+	public virtual void SaveDataToPath(bool collectParams, string key)
+	{
+		SaveData(collectParams, false);
+		SaveDataObject(key);
+	}
 
-    public void SaveDataImmediately()
-    {
-        if (coroutine != null) savingManager.StopCoroutine(coroutine);
-        SaveDataObject();
-    }
+	public void SaveDataImmediately()
+	{
+		if (coroutine != null) savingManager.StopCoroutine(coroutine);
+		SaveDataObject();
+	}
 
-    public virtual void LoadData() { }
-    public virtual bool IsDataEmpty() { return true; }
-    public abstract void ResetData(int flag = 0);
-    protected abstract void SaveDataObject();
+	public virtual void LoadData() { }
+	public virtual bool IsDataEmpty() { return true; }
+	public abstract void ResetData(int flag = 0);
+	protected abstract void SaveDataObject();
 
-    protected virtual void SaveDataObject(string key)
-    {
-        //Just dublicate it in override method (except ResourceProducerSavingData)
-        ES3.Save(ToString(), this, $"SaveFile_{key}.es3");
-    }
+	protected virtual void SaveDataObject(string key)
+	{
+		//Just dublicate it in override method (except ResourceProducerSavingData)
+		ES3.Save(ToString(), this, $"SaveFile_{key}.es3");
+	}
 }
